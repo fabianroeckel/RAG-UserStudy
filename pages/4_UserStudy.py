92f771fc18e94b372c191a3d77e1105df7efb3f8
fabi-roeckel <fabian.roeckel@glocally.de>
2024-06-08 20:49:34 +0200

    Updated error-text condition

--- pages/4_UserStudy.py
+++ pages/4_UserStudy.py
@@ -162,9 +162,7 @@ try:
             if st.form_submit_button():
                 if int(decision[0]) < 1 or error == "Dummy" :
                     st.error("You need to answer all the questions (Decision, Trust and Error-Detection!)")
-                if error == "Yes" and error_text == "":
-                    st.error("Specify the text passage where you identified the error")
-                if (int(decision[0]) > 0 and error != "Dummy" ) and not (error == "Yes" and error_text == ""):
+                if (int(decision[0]) > 0 and error != "Dummy" ):
                     st.session_state.progress += 8
                     timeSpentPerTask = store_and_compute_time_difference("timestamp")
                     logger.info(f"Time spent on this taks: {timeSpentPerTask['time_difference']}")
